6ee44b35418cf7b5407227e1ff3055877fa45246
sohil-25 <sohilbakroliya2510@gmail.com>
2022-09-13 15:35:53 +0530

    Advance props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Stats } from 'fs';
 import './App.css';
 import { Greet } from './components/Greet';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
 
 function App() {
 
@@ -31,6 +35,12 @@ const nameList = [
       <Greet name="peter" message={10} isLoggedIn={false}  />
       <Person name={personName} />
       <PersonList names={nameList} />
+      <Status status='loading' />
+      <Heading>Advance props example</Heading>
+      <Oscar>
+        <Heading>Advance props example</Heading>
+      </Oscar>
+      <Greet name="Advance props example" isLoggedIn={true}  />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,16 +1,17 @@
 type GreetProps={
     name:string,
-    message:number,
+    message?:number,
     isLoggedIn:boolean,
 }
 
 export const Greet = (props : GreetProps) => {
+    const { message = 0 } = props
     return (
         <div>
             <h2>
                {props.isLoggedIn
                ?
-               `Welcome ${props.name}! you have ${props.message} new msg`
+               `Welcome ${props.name}! you have ${message} new msg`
                : 'Welcome Guest'
                }
             </h2>
