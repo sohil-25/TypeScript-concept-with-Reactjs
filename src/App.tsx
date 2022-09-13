78c593c9e7997b33fc1cf21b698aa276826083f0
sohil-25 <sohilbakroliya2510@gmail.com>
2022-09-13 12:12:10 +0530

    Basic Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+
+const personName = {
+  first: 'Tony',
+  last: 'Stark',
+}
+
+const nameList = [
+  {
+    first: 'Peter',
+    last: 'griffin1'
+  },
+  {
+    first: 'Stewie',
+    last: 'griffin2'
+  },
+  {
+    first: 'Meg',
+    last: 'griffin3'
+  },
+
+]
+
   return (
-    <Greet name="peter" />
+    <div className='App'>
+      <Greet name="peter" message={10} isLoggedIn={false}  />
+      <Person name={personName} />
+      <PersonList names={nameList} />
+    </div>
   );
 }
 
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,11 +1,19 @@
 type GreetProps={
-    name:string
+    name:string,
+    message:number,
+    isLoggedIn:boolean,
 }
 
 export const Greet = (props : GreetProps) => {
     return (
-        <div className="App">
-            <h2>Welcome {props.name}</h2>
+        <div>
+            <h2>
+               {props.isLoggedIn
+               ?
+               `Welcome ${props.name}! you have ${props.message} new msg`
+               : 'Welcome Guest'
+               }
+            </h2>
         </div>
     )
 }
