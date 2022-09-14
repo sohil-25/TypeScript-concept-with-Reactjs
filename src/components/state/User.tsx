222ef3e79791e8f16d24d6421929a285a76f3015
sohil-25 <sohilbakroliya2510@gmail.com>
2022-09-14 12:12:54 +0530

    useState type assertion

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -1,28 +1,44 @@
 import { useState } from "react";
 
-type AuthUser = {
-  name: string;
-  email: string;
+type AuthUser1 = {
+  name1: string;
+  email1: string;
 };
 
-export const User = () => {
-  const [user, setUser] = useState<AuthUser | null>(null);
+type AuthUser2 = {
+    name2: string;
+    email2: string;
+  };
 
-  const handleLogin = () => {
+export const User = () => {
+  const [user, setUser] = useState<AuthUser1 | null>(null);
+  const [user2, setUser2] = useState<AuthUser2>({} as AuthUser2);
+
+
+  const handleLogin1 = () => {
     setUser({
-      name: "sohil",
-      email: "sohil@gmail.com",
+      name1: "sohil",
+      email1: "sohil@gmail.com",
     });
   };
+  const handleLogin2 = () => {
+    setUser2({
+      name2: "sohil2",
+      email2: "sohil2@gmail.com",
+    });
+  };
+
   const handleLogout = () => {
     setUser(null);
   };
   return (
     <div>
-      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogin1}>Login1</button>
       <button onClick={handleLogout}>Log out</button>
-      <div>User name is {user?.name}</div>
-      <div>User email is {user?.email}</div>
+      <div>User name is {user?.name1}</div>
+      <div>User email is {user?.email1}</div>
+      <button onClick={handleLogin2}>Login2</button>
+      <div>User2 name - {user2.name2} email- {user2.email2}</div>
     </div>
   );
 };
